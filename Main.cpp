308c1f29d72b19fd972fb6b599d4d95defaddb53
nugroho <massatriya@gmail.com>
2017-04-19 07:15:12 +0700

    added trees

--- Main.cpp
+++ Main.cpp
@@ -8,6 +8,8 @@
 #define red 255,0,0
 #define yellow 255,255,0
 #define green 0,255,0
+#define dark_green 1, 127, 5
+#define light_green 0, 224, 7
 
 #define ymaks 800
 #define xmaks 1200
@@ -46,13 +48,34 @@ int rainbowX(int y){
 };
 
 void drawTree(int awal, int batas){
-	glBegin(GL_LINES);
-	for(int x = awal;x < batas;x++){
-		glColor3ub(139,69,19);
-		glVertex2f(x,10);
-		glVertex2f(x,360);
-	}
+	int height = 600;
+	int x[] = {
+		awal,800,
+		awal,height,
+		batas,800,
+		batas,800,
+		batas,height,
+		awal,height
+	};
+	glColor3ub(139,69,19);
+	drawTriangles(x,6,'t');
+	int leaves[] = {
+		awal-50,height,
+		batas+50,height,
+		((batas-awal)/2)+awal,height-100
+	};
+	glBegin(GL_TRIANGLES);
+	glColor3ub(dark_green);
+	glVertex2f(awal-50,ymaks-height);
+	glColor3ub(light_green);
+	glVertex2f(((batas-awal)/2)+awal,ymaks-(height-200));
+	glVertex2f(batas+50,ymaks-height);
 	glEnd();
+	// for(int x = awal;x < batas;x++){
+	//
+	// 	glVertex2f(x,0);
+	// 	glVertex2f(x,360);
+	// }
 }
 
 
@@ -170,8 +193,6 @@ void matahari(GLfloat x, GLfloat y, GLfloat radius){
 	GLfloat rdalam = radius + 10;
 
 	GLfloat twicePi = 2.0f * PI;
-	x = xmaks / 2;
-	y = ymaks / 2;
 
 	glColor3ub(246, 210, 52);
 
@@ -219,9 +240,11 @@ void Draw() {
 	glColor3ub(76, 159, 247);
 	drawBackground();
 	drawRainbow(15);
-	matahari(xmaks / 2, ymaks / 2, 100);
+	matahari(xmaks, ymaks, 100);
 	//radialGradientCircle(50,20);
-	drawTree(150, 300);
+	drawTree(150, 200);
+	drawTree(250, 300);
+	drawTree(350, 400);
 	glFlush();
 }
 
